965743cd6db75c9e8ec0841a2b07c74b1eb465f1
Quentin Hill <quentin@quentinhill.com>
2011-04-23 21:34:11 -0700

    Commented existing files

--- Curator/bootstrap.php
+++ Curator/bootstrap.php
@@ -1,29 +1,62 @@
 <?php
+/**
+ * Bootstrap, the Go Getter.
+ * 
+ * The bootstrap does the nitty gritty, bare-bones stuff. Without which, we are nothing.
+ * 
+ * @author Quentin Hill <quentin@quentinhill.com>
+ * @package curator
+ * @subpackage Curator
+ */
 
+/**
+ * Defines the absolute path to the `Curator/` directory.
+ */
 if( !defined('CURATOR_DIR') ) {
 	define('CURATOR_DIR', ROOT_DIR.DS.'Curator');
 }
 
+/**
+ * Defines the absolute path to the `Curator/Configs/` directory.
+ */
 if( !defined('CURATOR_CONFIGS_DIR') ) {
 	define('CURATOR_CONFIGS_DIR', CURATOR_DIR.DS.'Configs');
 }
 
+/**
+ * Defines the absolute path to the `Curator/Library` directory.
+ */
 if( !defined('CURATOR_LIBRARY_DIR') ) {
 	define('CURATOR_LIBRARY_DIR', CURATOR_DIR.DS.'Library');
 }
 
+/**
+ * Defines the absolute path to the `Curator/Skeleton` directory.
+ */
 if( !defined('CURATOR_SKELETON_DIR') ) {
 	define('CURATOR_SKELETON_DIR', CURATOR_DIR.DS.'Skeleton');
 }
 
+/**
+ * Defines the absolute path to the `Curator/Temporary` directory.
+ */
 if( !defined('CURATOR_TEMPORARY_DIR') ) {
 	define('CURATOR_TEMPORARY_DIR', CURATOR_DIR.DS.'Temporary');
 }
 
+/**
+ * Defines the absolute path to the `Vendor` directory.
+ */
 if( !defined('CURATOR_VENDOR_DIR') ) {
 	define('CURATOR_VENDOR_DIR', CURATOR_DIR.DS.'Vendor');
 }
 
+/**
+ * Special autoload callback function. Allows us to skip most uses of include/require/etc..
+ * 
+ * @param string $class_name The name of the class to load.
+ * @return void
+ */
 function curator_autoload($class_name)
 {
 	$class_file_name = strtolower($class_name).'.php';
@@ -34,4 +67,5 @@ function curator_autoload($class_name)
 	}
 }
 
+// register our callback
 spl_autoload_register('curator_autoload');
--- bin/curator.php
+++ bin/curator.php
@@ -1,14 +1,28 @@
 #!/usr/bin/env php
 <?php
+/**
+ * Curator, the Face.
+ * 
+ * Curator is our core front-end to the user.
+ * 
+ * @author Quentin Hill <quentin@quentinhill.com>
+ * @package curator
+ * @subpackage bin
+ */
 
+/**
+ * Defines shorthand notation for DIRECTORY_SEPARATOR.
+ */
 if( !defined('DS') ) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
 
+/**
+ * Defines the absolute path to the root of our project.
+ */
 if( !defined('ROOT_DIR') ) {
 	define('ROOT_DIR', dirname(dirname(__FILE__)));
 }
 
+// Load the bootstrap.
 require_once(ROOT_DIR.DS.'Curator'.DS.'bootstrap.php');
-
-Config::load();
