e5ab48c2c4c4782d58fa20f612eaf6132305bc73
Quentin Hill <quentin@quentinhill.com>
2011-04-23 20:57:42 -0700

    Introduced basic filesystem awareness

--- bin/curator.php
+++ bin/curator.php
@@ -1,3 +1,14 @@
 #!/usr/bin/env php
 <?php
 
+if( !defined('DS') ) {
+	define('DS', DIRECTORY_SEPARATOR);
+}
+
+if( !defined('ROOT_DIR') ) {
+	define('ROOT_DIR', dirname(dirname(__FILE__)));
+}
+
+if( !defined('CURATOR_DIR') ) {
+	define('CURATOR_DIR', ROOT_DIR.DS.'Curator');
+}
