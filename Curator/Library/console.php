8275e93f2ca8fcfbf7f1a717f17873b0e90f3696
Quentin Hill <quentin@quentinhill.com>
2011-04-24 18:12:17 -0700

    Added base console class

---
+++ Curator/Library/console.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Console, the Fast Talker.
+ * 
+ * The console presents information to the user, and gets information back.
+ * 
+ * @author       Quentin Hill <quentin@quentinhill.com>
+ * @copyright    Copyright © 2011 Quentin Hill. Some Rights Reserved.
+ * @link         http://quentinhill.github.com/curator
+ * @license      http://creativecommons.org/licenses/by-sa/3.0/
+ * @package      Curator
+ * @subpackage   Library
+ */
+
+/**
+ * Console class.
+ *
+ * @package Curator
+ * @subpackage Library
+ */
+class Console extends Object
+{
+	// Hold an instance of the class
+	private static $instance;
+	
+	/**
+	 * There can be only one Console.
+	 * 
+	 * @return Console
+	 * @access public
+	 */
+	public static function Singleton() 
+	{
+		if (!isset(self::$instance)) {
+			$c = __CLASS__;
+			self::$instance = new $c;
+		}
+		
+		return self::$instance;
+	}
+	
+	/**
+	 * A private constructor; prevents direct creation of object
+	 * 
+	 * @return Console
+	 * @access private
+	 */
+	private function __construct() 
+	{
+		$this->args = $_SERVER['argv'];
+	}
+	
+	/**
+	 * Prevent the Curator from being cloned.
+	 * 
+	 * Triggers an E_USER_ERROR.
+	 * @return void
+	 * @access public
+	 */
+    public function __clone()
+    {
+        trigger_error('Clone is not allowed.', E_USER_ERROR);
+    }
+	
+	/**
+	 * Clears the console.
+	 * @return void
+	 * @access public
+	 */
+	public function clear()
+	{
+		passthru('clear');
+	}
+	
+	/**
+	 * Prints a prompt to stdout, and reads from stdin.
+	 * @param string $prompt What to ask the user.
+	 * @return string The user's input.
+	 * @access public
+	 */
+	public function input($prompt)
+	{
+		$this->stdout($prompt."\n".'> ', false);
+		
+		$result = fgets(STDIN);
+		
+		if( $result === false ) {
+			exit;
+		}
+		
+		$result = trim($result);
+		
+		return $result;
+	}
+	
+	/**
+	 * Prints a string to stdout.
+	 * @param string $string The string to print.
+	 * @param boolean $newline If true, a newline is added to $string before printing.
+	 * @return void
+	 * @access public
+	 */
+	public function stdout($string, $newline = false)
+	{
+		if( $newline === false ) {
+			return fwrite(STDOUT, $string);
+		} else {
+			return fwrite(STDOUT, $string."\n");
+		}
+	}
+	
+	/**
+	 * Prints a string to stderr.
+	 * @param string $string The string to print.
+	 * @param boolean $newline If true, a newline is added to $string before printing.
+	 * @return void
+	 * @access public
+	 */
+	public function stderr($string, $newline = false)
+	{
+		if( $newline === false ) {
+			return fwrite(STDERR, $string);
+		} else {
+			return fwrite(STDERR, $string."\n");
+		}
+	}
+}
