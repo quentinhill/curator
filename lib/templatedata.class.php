24e30fbbd41e728d61ccdb09edc34c66c64bdd43
Quentin Hill <quentin@quentinhill.com>
2011-05-17 15:06:04 -0700

    Now values are shared amongst instances.

--- lib/templatedata.class.php
+++ lib/templatedata.class.php
@@ -16,39 +16,15 @@
  * @subpackage	teamplates
  * @author		Quentin Hill <quentin@quentinhill.com>
  */
-class TemplateData implements \ArrayAccess
+class TemplateData
 {
-	/**
-	 * Singleton instance.
-	 *
-	 * @var object
-	 * @access private
-	 */
-	private static $instance;
-	
 	/**
 	 * The backend array.
 	 *
 	 * @var array
-	 * @access private
+	 * @access protected
 	 */
-	private $dictionary = null;
-	
-	/**
-	 * Returns the shared TemplateData.
-	 *
-	 * @return TemplateData
-	 * @access public
-	 */
-	public static function singleton() 
-	{
-		if( !isset(self::$instance) ) {
-			$c = __CLASS__;
-			self::$instance = new $c;
-		}
-		
-		return self::$instance;
-	}
+	protected static $data = array();
 	
 	/**
 	 * Private constructor.
@@ -56,51 +32,27 @@ class TemplateData implements \ArrayAccess
 	 * @return Console
 	 * @access private
 	 */
-	private function __construct() 
+	public function __construct() 
 	{
-		$this->dictionary = array();
-		
 		$this->loadDefaultValues();
-	}
-	
-	/**
-	 * Triggers an error.
-	 *
-	 * @access public
-	 */
-	public function __clone()
-	{
-		trigger_error('Cloning TemplateData is not allowed.', E_USER_ERROR);
 	}
 	
 	private function loadDefaultValues()
 	{
 		$config = new Config();
 		
-		$this->dictionary = $config->loadData(CURATOR_CONFIG_DIR.DS.'templatedata.yml');
+		TemplateData::$data = $config->loadData(CURATOR_CONFIG_DIR.DS.'templatedata.yml');
 	}
 	
-	public function offsetSet($offset, $value)
+	public function getValue($group, $key)
 	{
-		if( is_null($offset) ) {
-			$this->container[] = $value;
-		} else {
-			$this->container[$offset] = $value;
-		}
+		$value = TemplateData::$data[$group][$key];
+		
+		return $value;
 	}
 	
-	public function offsetExists($offset)
+	public function setValue($group, $key, $value)
 	{
-	    return isset($this->container[$offset]);
-	}
-	
-	public function offsetUnset($offset)
-	{
-	    unset($this->container[$offset]);
-	}
-	
-	public function offsetGet($offset)
-	{
-	    return isset($this->container[$offset]) ? $this->container[$offset] : null;
+		TemplateData::$data[$group][$key] = $value;
 	}
 }
